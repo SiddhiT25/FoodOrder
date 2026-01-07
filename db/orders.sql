bc56e5afcd7fe823401b119038ada07eca7529d2
SiddhiT25 <siddhitripathipnk@gmail.com>
2026-01-07 22:02:06 +0530

    added order table
    added order table

--- db/orders.sql
+++ db/orders.sql
@@ -1,1 +1,9 @@
-SELECT * FROM foodorderdb.orders;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `item` varchar(50) DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `total` int DEFAULT NULL,
+  `order_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
